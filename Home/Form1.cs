0186633c0d3d81025d6893daeffd357e65fa9333
Tsahi78 <tsahi78@gmail.com>
2016-05-17 11:39:28 +0300

    להשלים עבודה

---
+++ Home/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Home
+{
+    public partial class Form1 : Form
+    {
+           
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Pen myPen;
+            Brush myBrush;
+            myPen = new Pen(Color.Black, 2);
+            
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawLine(Pens.Black, 50, 300, 300, 300);
+        }
+
+     
+    }
+}
